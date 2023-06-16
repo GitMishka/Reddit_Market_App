7b60d8fa707a79fdebca9a43960ed1aec8e85631
Misha <mishagrigoryan@gmail.com>
2023-06-15 20:30:49 -0400

    table_creator

---
+++ quick_table_crtr.py
@@ -0,0 +1,27 @@
+import praw
+import pandas as pd
+import time
+import psycopg2
+import requests
+from twilio.rest import Client
+import config
+
+conn = psycopg2.connect(
+    host=config.pg_host,
+    database=config.pg_database,
+    user=config.pg_user,
+    password=config.pg_password
+)
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS termlog (
+        id TEXT PRIMARY KEY,
+        term TEXT,
+        post_time TIMESTAMP
+    )
+""")
+
+conn.commit()
+cur.close()
+conn.close()
